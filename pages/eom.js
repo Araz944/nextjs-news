30d3715cc194c0ab359d9addf84846abeadc9208
Araz944 <araz944memmedov@gmail.com>
2021-08-26 17:41:34 +0400

    added <Head>

--- pages/eom.js
+++ pages/eom.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import styles from "../styles/EOM.module.css";
 import { Toolbar } from "../components/toolbar";
 
@@ -6,18 +7,40 @@ const EOM = ({ employee }) => {
   console.log(myData);
 
   return (
-    <div className="page-container">
-      <Toolbar />
-      <div className={styles.main}>
-        <h1>Employee Of The Month</h1>
-        <div className={styles.employeeOfTheMonth}>
-          <h3>{myData.name}</h3>
-          <h6>{myData.position}</h6>
-          <img src={myData.imageURL} alt="Picture of the author" />
-          <p>{myData.description}</p>
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name="description"
+          content={`This month's employee is ${employee.name}`}
+        />
+        <meta property="og:image" content={employee.imageURL} />
+        <meta property="og:title" content="Employee Of The Month" />
+        <meta
+          property="og:description"
+          content={`The author of this page is ${employee.name}`}
+        />
+        <meta property="linkedIn:image" content={employee.imageURL} />
+        <meta property="linkedIn:title" content="Employee Of The Month" />
+        <meta
+          property="linkedIn:description"
+          content={`This months employee is ${employee.name}`}
+        />
+      </Head>
+
+      <div className="page-container">
+        <Toolbar />
+        <div className={styles.main}>
+          <h1>Employee Of The Month</h1>
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{myData.name}</h3>
+            <h6>{myData.position}</h6>
+            <img src={myData.imageURL} alt="Picture of the author" />
+            <p>{myData.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
